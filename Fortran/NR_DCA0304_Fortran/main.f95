c33bdf46df59a5446646f066537b72d7efc8da5d
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2018-11-08 13:19:21 -0300

    Update main.f95

--- Fortran/NR_DCA0304_Fortran/main.f95
+++ Fortran/NR_DCA0304_Fortran/main.f95
@@ -2,62 +2,48 @@
 ! By WQY
 
 program main
-  !implicit none
-  !integer re_i
-  !write(*,*) "Hello World!"
-  !re_i = system("pause")
-
     implicit none
-    real :: x0(2)
-    real, dimension(:), allocatable :: x
+    real :: x0(2), x(size(x0))
 
+    
+    
     x0(1) = 0
     x0(2) = 2
+
+
+    
+
 
     x = newton_raph(x0, 1e-12, .true., 100)
     write(*,*) x(1), x(2)
 
-    write(*,*) x(1), x(2), x(3)
+
 
     !x0(1) = 1
     !x0(2) = 2
     !x = newton_raph(x0, 1e-12, .true., 100)
 
-
 contains
-    function trans(b) result(t)
-        integer :: n, i
-        real, dimension(:) :: b
-        real, dimension(:,:), allocatable :: t
-        n = size(b)
-        allocate(t(n, 1))
-        do i = 1,n
-            t(i, 1) = b(i)
-        end do
-    end function trans
 
     function f(p) result(res)
-        !sistema de equações
+        IMPLICIT NONE
+        !sistema de equaÃ§Ãµes
         !eq1: x + y ^ 2 = 4
         !eq2: e ^ x + xy = 3
-        real, dimension(:) :: p
-        real, dimension(:), allocatable :: res
-        integer :: n
-        n = size(p)
+        real :: p(:), res(size(p))
 
         res(1) = p(1)+(p(2)**2)-4
         res(2) = exp(p(1))+p(1)*p(2)-3
     end function f
 
     function jac(x) result(J)
-        real :: dx=1e-10
-        real, dimension(:) :: x
-        real, dimension(:,:), allocatable :: J
+        IMPLICIT NONE
+        real :: dx=1e-10, x(:), J(size(x), size(x))
         real, dimension(:), allocatable :: xn, dy, dif
         integer :: n, i, k
 
         n = size(x)
-        allocate(J(n, n), xn(n), dy(n), dif(n))
+        allocate(xn(n), dy(n), dif(n))
         do k = 1,n
             xn = x
             x(k) = xn(k)+dx
@@ -70,13 +56,11 @@ contains
     end function jac
 
     function solve(M, b) result(x)
-        real, dimension(:) :: b, M(:, :)
-        real, dimension(:), allocatable :: x
-        real :: lambda=1, es=1e-6, dummy, soma, old, ea
+        IMPLICIT NONE
+        real :: b(:), M(:, :), x(size(b)), lambda=1, es=1e-6, dummy, soma, old, ea
         integer :: n, iter, sentinela, i, j
 
         n = size(b)
-        allocate(x(n))
 
         do i = 1,n
             x(i) = 0
@@ -125,16 +109,16 @@ contains
     end function solve
 
     function newton_raph(x0, tol, iter, n_tot) result(x)
-        real, dimension(:) :: x0
-        real, dimension(:), allocatable :: x, e, F, S
-        real :: tol
+        IMPLICIT NONE
+        real :: x0(:), x(size(x0)), tol, e, temp(2)
+        real, dimension(:), allocatable :: func, S, jc(:, :)
         integer :: n_tot, n0, n=0, i, j, k
         logical :: iter
 
         n0 = size(x0)
         tol = abs(tol)
         n_tot = abs(n_tot)
-        allocate(x(n0), e(n0), F(n0), S(n0))
+        allocate(jc(n0, n0), func(n0), S(n0))
         do i = 1,n0
             x(i) = real(x0(i))
         end do
@@ -142,22 +126,23 @@ contains
 
         do while ((e>tol).and.(n<=n_tot))
             n = n+1
-            F = f(x)
-            e = maxval(abs(F))
-            J = jac(x)
-            if (size(F)==1) then
-                x = x - (F/J)
+            func = f(x)
+            e = maxval(abs(func))
+            jc = jac(x)
+            if (size(func)==1) then
+                x = x - (func(1)/jc(1, 1))
             else
-                S = solve(J, -F)
+                S = solve(jc, -func)
                 x = x+S
             end if
         end do
 
         if (iter .eqv. .true.) then
-            write(*,*) "Total de iterações: ", n
+            write(*,*) "Total de iteraÃ§Ãµes: ", n
         end if
         if (n>=n_tot) then
-            write(*,*) "Processo parou, número de iterações limite atingido", n
+            write(*,*) "Processo parou, nÃºmero de iteraÃ§Ãµes limite atingido", n
         end if
     end function newton_raph
+
 end program main
