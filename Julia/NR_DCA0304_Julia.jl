98595f99b21fef8a2881358c57fb1d10f6e56b0a
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2018-11-11 20:37:07 -0300

    Update NR_DCA0304_Julia.jl

--- Julia/NR_DCA0304_Julia.jl
+++ Julia/NR_DCA0304_Julia.jl
@@ -1,42 +1,14 @@
-
-
-function solve_sis(M, b)
-    n = length(b)
-    x = zeros(n)
-    xo = zeros(n)
-    del=1e-6
-    intera = 10
-    k = 0
-    test = 1
-
-    while (k<intera)&(test>del)
-        k = k+1
-        xo = x #Jacobi
-        for i = 1:n
-            summ = 0;
-            for j = 1:n
-                summ = summ + M[i, j]*xo[j]*(i!=j)
-            end
-            x[i] = (b[i]-summ)/M[i, i]
-            test = x[i]-xo[i]
-        end
-    end
-    return x
-end
-
 function LU(matriz, vetor_b)
     n = length(vetor_b)  # Give us total of lines
-    y = zeros(n)
     x = zeros(n)
     # (2) Fill L matrix and its diagonal with 1
     L = zeros(n, n)
     for i = 1:n
         L[i, i] = 1
     end
-
     # (3) Fill U matrix
+    U=zeros(n,n)
     U = matriz
-    
     
     # (4) Find both U and L matrices
     for i = 1:n-1
@@ -52,25 +24,32 @@ function LU(matriz, vetor_b)
             U[k, i] = 0
         end
     end
-        
+    #print(L)
+    y = zeros(n)
     # (5) Perform substitution Ly=b
     for i = 1:n
         y[i] = vetor_b[i] / L[i, i]
-        for k = 1:i
-            y[i] = y[i] - y[k]*L[i, k]
+        
+        for k = 1:i-1
+            y[i] = y[i]  - y[k]*L[i, k]
+            #print(y[i], "\n")
         end
         #print("y[", i, "] =", y[i], "\n")
     end
+    #print(y, "\n")
+    n = length(y)
+    #print(n)
     x = copy(y)
     #print("\n", x, "\n")
     # (6) Perform substitution Ux=y
-    for i = n-1:-1:1
-        for k = i+1:n
+    for i = (n:-1:1)
+    
+        for k = 1+i:n
             x[i] = x[i] - x[k]*U[i, k]
         end
         x[i] = x[i]/U[i, i]
     end
-
+    #print(x, "\n")
 
     return x
 end
@@ -82,7 +61,7 @@ function f(p)
     a = p[2] + p[3] - exp(-p[1])
     b = p[1] + p[3] - exp(-p[3])
     c = p[1] + p[2] - exp(-p[3])
-    return [a b c]
+    return [a c b]
 end
 
 function jac(x, dx=1e-10)
@@ -129,6 +108,6 @@ function newton_raph(x0, tol, iter, n_tot)
 end
 
 
-    x0 = [0.5, 0.5, 0.5]
-    x = newton_raph(x0, 1e-6, true, 1)
-    print(x)
+    x0 = [0.5, 1, 5]
+    x = newton_raph(x0, 1e-12, true, 100)
+    print("\nSolução= ",x, "\n")
