6856d64d7121125f76309c1d8e77c12ad3a5f2ba
Levy Gabriel <30806075+Vylvlur@users.noreply.github.com>
2018-11-12 22:00:24 -0300

    Update main.f95

--- Fortran/NR_DCA0304_Fortran/main.f95
+++ Fortran/NR_DCA0304_Fortran/main.f95
@@ -1,37 +1,46 @@
-! A fortran95 program for G95
-! By WQY
-
 program main
     implicit none
-    double precision :: x0(3), x(size(x0)), tol=1e-12
-
+    double precision :: x0(3), x(size(x0)), tol, iter
+    !Chute inicial
     x0(1) = 0.5
     x0(2) = 0.5
     x0(3) = 0.5
+    iter = 100 !Numero de iteracoes
+    tol = 1e-12 !Tolerancia
 
-    x = newton_raph(x0, tol, .true., 100)
-    write(*,*) x(1), x(2), x(3)
+    x = newton_raph(x0, tol, .true., iter)
+    write(*,*) x(1), x(2), x(3) !Solucao
 
 
 
 contains
-
+    
+    !DECLARACAO DAS FUNCOES
     function f(p) result(res)
         IMPLICIT NONE
         double precision :: p(:), res(size(p))
+        
+        !Sistema de equacoes
+
+        !eq1: 1/2*sin(x1*x2)-(x2/(4*pi))-(x1/2),
+        !eq2: (1-1/(4*pi))*((exp(2*x1))-exp(1))-((exp(1)*x2)/pi)-2*exp(1)*x1)
+
+        !eq3: x2+x3-exp(-x1)
+        !eq4: x1+x3-exp(-x3)
+        !eq5: x1+x2-exp(-x3)
 
         res(1) = p(2) + p(3) - exp(-p(1))
         res(2) = p(1) + p(3) - exp(-p(3))
         res(3) = p(1) + p(2) - exp(-p(3))
     end function f
-
+    !MATRIZ JACOBIANA
     function jac(x) result(jc)
         IMPLICIT NONE
         double precision :: dx=1e-12, x(:), jc(size(x), size(x)), xn(size(x)), dy(size(x)), dif(size(x))
         integer :: n, i, k
 
         n = size(x)
-        do k = 1,n
+        do k = 1,n !Calculo numerico da matriz jacobiana
             xn = x
             xn(k) = xn(k)+dx
             dy = f(xn) - f(x)
@@ -41,7 +50,7 @@ contains
             end do
         end do
     end function jac
-
+    ! METODO DA FATORACAO LU
     function LU(M, b) result(x)
         IMPLICIT NONE
         double precision :: b(:), M(:, :), x(size(b)), y(size(b)), U(size(b), size(b)), L(size(b), size(b)), c
@@ -58,24 +67,26 @@ contains
         do i = 1,n
             x(i) = 0
             y(i) = 0
-            L(i, i) = 1
+            L(i, i) = 1 !Preenche L com a Matriz Identidade
         end do
 
-        U = M
+        U = M !Atribui a Matriz J a Matriz U
 
         do i = 1,(n-1)
             do k = (i+1),n
                 c = U(i, k) / U(i, i)
-                L(k, i) = c
+                L(k, i) = c !Armazena o multiplicador
                 do j = 1,n
-                    U(k, j) = U(k, j) - c*U(i, j)
+                    U(k, j) = U(k, j) - c*U(i, j) !Multiplica com o pivo da linha e subtrai
+end
                 end do
             end do
             do k = (i+1),n
                 U(k, i) = 0
             end do
         end do
-
+        
+        !Resolve o Sistema Ly=b
         do i = 1,n
             y(i) = b(i) / L(i, i)
             do k = 1,(i-1)
@@ -84,7 +95,7 @@ contains
         end do
 
         x = y
-
+        !Resolve o Sistema Ux=y
         do i = n,1,-1
             do k = (i+1),n
                 x(i) = x(i) - x(k)*U(i, k)
@@ -92,7 +103,7 @@ contains
             x(i) = x(i)/U(i, i)
         end do
     end function LU
-
+    !METODO NEWTON_RAPHSON
     function newton_raph(x0, tol, iter, n_tot) result(x)
         IMPLICIT NONE
         double precision :: x0(:), x(size(x0)), tol, e, func(size(x0)), S(size(x0)), jc(size(x0), size(x0))
